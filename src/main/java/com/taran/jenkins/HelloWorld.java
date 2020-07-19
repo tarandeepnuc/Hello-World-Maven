23b91d76052b8133376565808d06f762e08890b9
savneet sabharwal <tarandeepsingh.sabharwal@tcs.com>
2020-07-18 23:11:46 -0400

    New maven project

---
+++ src/main/java/com/taran/jenkins/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.taran.jenkins;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cleaning Project");
+	}
+
+}
